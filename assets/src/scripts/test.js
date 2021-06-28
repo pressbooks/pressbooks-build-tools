5b74ac33338bbe7d75b3b8e139d78ea29a848bb5
Ned Zimmerman <ned@bight.dev>
2021-06-27 23:05:43 -0300

    feat: upgrade to Laravel Mix 6.x

--- assets/src/scripts/test.js
+++ assets/src/scripts/test.js
@@ -1,10 +1,15 @@
 jQuery( function ( $ ) {
-
+	/**
+	 *
+	 * @param {number} n The input number.
+	 * @returns {number} The next number in the Fibonacci sequence.
+	 */
 	function fibonacci( n ) {
 		return n < 1 ? 0
 			: n <= 2 ? 1
 				: fibonacci( n - 1 ) + fibonacci( n - 2 );
 	}
+
 	fibonacci( 4 );
 
 } );
